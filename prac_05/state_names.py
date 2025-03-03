a289cd092895eb029c325f96081ca7e8473ccf40
DastanRozakhun <dastan.rozakhun@my.jcu.edu.au>
2025-03-04 00:02:51 +0800

    Fixed code

--- prac_05/state_names.py
+++ prac_05/state_names.py
@@ -1,18 +1,29 @@
 """
 CP1404/CP5632 Practical
 State names in a dictionary
-File needs reformatting
+File reformatted to follow PEP 8 conventions
 """
 
-# TODO: Reformat this file so the dictionary code follows PEP 8 convention
-CODE_TO_NAME = {"QLD": "Queensland", "NSW": "New South Wales", "NT": "Northern Territory", "WA": "Western Australia",
-                "ACT": "Australian Capital Territory", "VIC": "Victoria", "TAS": "Tasmania", "SA": "South Australia"}
-print(CODE_TO_NAME)
+CODE_TO_NAME = {
+    "QLD": "Queensland",
+    "NSW": "New South Wales",
+    "NT": "Northern Territory",
+    "WA": "Western Australia",
+    "ACT": "Australian Capital Territory",
+    "VIC": "Victoria",
+    "TAS": "Tasmania",
+    "SA": "South Australia"
+}
 
-state_code = input("Enter short state: ")
+# Print all state codes and names
+for code, name in CODE_TO_NAME.items():
+    print(f"{code:3} is {name}")
+
+# Look up state names
+state_code = input("Enter short state: ").upper()
 while state_code != "":
-    if state_code in CODE_TO_NAME:
-        print(state_code, "is", CODE_TO_NAME[state_code])
-    else:
+    try:
+        print(f"{state_code} is {CODE_TO_NAME[state_code]}")
+    except KeyError:
         print("Invalid short state")
-    state_code = input("Enter short state: ")
+    state_code = input("Enter short state: ").upper()
